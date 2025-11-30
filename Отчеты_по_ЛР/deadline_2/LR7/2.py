543c20044d05e47e262588aaab0ee3cefec75e9e
RobukLeonid <robukleonid6@gmail.com>
2025-11-30 22:06:24 +0500

    Update 2.py

--- Отчеты_по_ЛР/deadline_2/LR7/2.py
+++ Отчеты_по_ЛР/deadline_2/LR7/2.py
@@ -1,11 +1,1 @@
-text = input("введите произвольный текст: ")
-stroki = input("введите две строки через пробел: ")
 
-strings = stroki.split()
-
-if len(strings) == 2:
-    string1, string2 = strings
-    new_text = text.replace(string1, string2)
-    print(f"новый текст: {new_text}")
-else:
-    print("нужно ввести ровно две строки через пробел")
